e8257e0e09d7b75518856da8412a81449e96a1fd
Tal Amir <tal.amir.99@gmail.com>
2020-07-23 22:25:42 +0300

    Delete nonSparsityRel.m

--- utils/nonSparsityRel.m
+++
@@ -1,9 +0,0 @@
-function out = nonSparsityRel(x,k)
-%function out = nonSparsityRel(x,k)
-%
-% Tells how far a vector x is from being k-sparse, in terms of relative
-% l1-distance.
-
-n = numel(x);
-out = norm(x - truncVec(x,k),1) / norm(truncVec(x,k),1) * (k/(n-k));
-end
